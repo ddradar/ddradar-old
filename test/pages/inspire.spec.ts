120b22d0d373387cf691b2e087f7014329db1dd1
nogic <24802730+nogic1008@users.noreply.github.com>
2019-11-03 23:26:11 +0900

    remove unused files

--- test/pages/inspire.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Inspire from '@/pages/inspire.vue'
-
-describe('Card', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Inspire)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
