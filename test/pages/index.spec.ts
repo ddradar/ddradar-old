6e45c317ee7d6c9741feccc61b4f98f4d9e7e8cc
nogic <24802730+nogic1008@users.noreply.github.com>
2019-11-06 23:49:22 +0900

    Load buefy on IndexPage test

--- test/pages/index.spec.ts
+++ test/pages/index.spec.ts
@@ -1,9 +1,13 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Buefy from 'buefy';
 import Index from '@/pages/index.vue'
+
+const localVue = createLocalVue();
+localVue.use(Buefy);
 
 describe('Card', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Index)
+    const wrapper = shallowMount(Index,{localVue})
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
