fea48ea0d7fd2ffaeebe8bb4ed29c99ed298de16
nogic <24802730+nogic1008@users.noreply.github.com>
2019-11-07 21:14:49 +0900

    Lint files

--- test/pages/index.spec.ts
+++ test/pages/index.spec.ts
@@ -1,13 +1,13 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
-import Buefy from 'buefy';
+import Buefy from 'buefy'
 import Index from '@/pages/index.vue'
 
-const localVue = createLocalVue();
-localVue.use(Buefy);
+const localVue = createLocalVue()
+localVue.use(Buefy)
 
 describe('Card', () => {
   test('is a Vue instance', () => {
-    const wrapper = shallowMount(Index,{localVue})
+    const wrapper = shallowMount(Index, { localVue })
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
