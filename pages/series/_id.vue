8ef1c506a848a063e8fac8595b255ecd3e7df9d5
nogic <24802730+nogic1008@users.noreply.github.com>
2019-11-12 00:22:22 +0900

    add data test with mock

--- pages/series/_id.vue
+++ pages/series/_id.vue
@@ -15,13 +15,13 @@
       </b-button>
     </div>
     <p class="subtitle">{{ message }}</p>
-    <SongList v-if="selected != null" :loading="isLoading" :songs="songs" />
+    <song-list v-if="selected != null" :loading="isLoading" :songs="songs" />
   </section>
 </template>
 
 <script lang="ts">
 import { Context } from '@nuxt/types'
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component } from 'nuxt-property-decorator'
 import { Song } from '@/types/song'
 import { SeriesList, getSeriesName } from '@/types/series'
 import { fetchSongs } from '@/plugins/song-repository'
@@ -29,10 +29,37 @@ import { fetchSongs } from '@/plugins/song-repository'
 @Component({
   components: {
     SongList: () => import('~/components/SongList.vue')
-  },
+  }
+})
+export default class SeriesPage extends Vue {
+  selected: number | null = null
+  songs: Song[] = []
+  isLoading = true
+  seriesList = SeriesList
+
+  public get pageTitle() {
+    if (
+      this.selected === null ||
+      !Number.isInteger(this.selected) ||
+      this.selected < 0 ||
+      this.selected >= SeriesList.length
+    ) {
+      return 'シリーズから探す'
+    }
+    return getSeriesName(SeriesList[this.selected])
+  }
+
+  public get message() {
+    return this.selected === null
+      ? 'シリーズを選択してください'
+      : this.songs.length === 0
+      ? 'Not Found'
+      : `Found ${this.songs.length} songs`
+  }
+
   async asyncData({ params }: Context) {
     const index = Number.parseInt(params.id)
-    if (isNaN(index)) {
+    if (isNaN(index) || index >= SeriesList.length) {
       return {
         isLoading: false
       }
@@ -50,27 +77,6 @@ import { fetchSongs } from '@/plugins/song-repository'
         isLoading: false
       }
     }
-  }
-})
-export default class SeriesPage extends Vue {
-  selected: number | null = null
-  songs: Song[] = []
-  isLoading = true
-  seriesList = SeriesList
-
-  public get pageTitle() {
-    if (this.selected == null) {
-      return 'シリーズから探す'
-    }
-    return getSeriesName(SeriesList[this.selected])
-  }
-
-  public get message() {
-    return this.selected === null
-      ? 'シリーズを選択してください'
-      : this.songs.length === 0
-      ? 'Not Found'
-      : `Found ${this.songs.length} songs`
   }
 }
 </script>
--- test/pages/series/_id.spec.ts
+++ test/pages/series/_id.spec.ts
@@ -1,13 +1,94 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import { shallowMount, createLocalVue, Wrapper } from '@vue/test-utils'
 import Buefy from 'buefy'
+import { mocked } from 'ts-jest/utils'
 import SeriesPage from '@/pages/series/_id.vue'
+import { SeriesList } from '@/types/series'
+import * as repo from '@/plugins/song-repository'
+
+jest.mock('@/plugins/song-repository')
 
 const localVue = createLocalVue()
 localVue.use(Buefy)
 
-describe('/single/:id', () => {
+describe('/series/:id', () => {
+  let wrapper: Wrapper<SeriesPage>
+  beforeEach(() => {
+    wrapper = shallowMount(SeriesPage, { localVue, stubs: ['song-list'] })
+  })
   test('is a Vue instance', () => {
-    const wrapper = shallowMount(SeriesPage, { localVue })
     expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+  describe('asyncData method', () => {
+    const hasDataIndex = 0
+    const noDataIndex = 1
+    const throwErrorIndex = 2
+    beforeAll(() => {
+      mocked(repo).fetchSongs.mockImplementation((_name, series) => {
+        if (series === SeriesList[hasDataIndex]) {
+          return Promise.resolve([
+            {
+              name: 'PARANOiA',
+              nameKana: 'PARANOIA',
+              nameIndex: 25,
+              artist: '180',
+              series: '1st',
+              minBPM: 180,
+              maxBPM: 180
+            }
+          ])
+        }
+        if (series === SeriesList[noDataIndex]) {
+          return Promise.resolve([])
+        }
+        return Promise.reject(new Error('Mock test error'))
+      })
+    })
+    test('param is 0', async () => {
+      const data = await (wrapper.vm.$options as any).asyncData({
+        params: { id: 'hoge' }
+      })
+      wrapper.setData(data)
+      expect((wrapper.vm as any).selected).toBeNull()
+    })
+    test('param is 0', async () => {
+      const data = await (wrapper.vm.$options as any).asyncData({
+        params: { id: hasDataIndex.toString() }
+      })
+      wrapper.setData(data)
+      expect((wrapper.vm as any).selected).toBe(hasDataIndex)
+    })
+    test('param is 1', async () => {
+      const data = await (wrapper.vm.$options as any).asyncData({
+        params: { id: noDataIndex.toString() }
+      })
+      wrapper.setData(data)
+      expect((wrapper.vm as any).selected).toBe(noDataIndex)
+    })
+    test('param is 2', async () => {
+      const data = await (wrapper.vm.$options as any).asyncData({
+        params: { id: throwErrorIndex.toString() }
+      })
+      wrapper.setData(data)
+      expect((wrapper.vm as any).selected).toBeNull()
+    })
+  })
+  describe('pageTitle getter', () => {
+    test('returns "シリーズから探す" if series not selected', () => {
+      expect((wrapper.vm as any).pageTitle).toBe('シリーズから探す')
+    })
+    test('returns "シリーズから探す" if selected is out of range', () => {
+      for (const i of [-1, 100, 1.5, NaN, Infinity]) {
+        wrapper.setData({ selected: i })
+        expect((wrapper.vm as any).pageTitle).toBe('シリーズから探す')
+      }
+    })
+    test('returns Series title if selected', () => {
+      wrapper.setData({ selected: 0 })
+      expect((wrapper.vm as any).pageTitle).toBe(`DDR ${SeriesList[0]}`)
+      wrapper.setData({ selected: SeriesList.length - 1 })
+      expect((wrapper.vm as any).pageTitle).toBe(
+        `DDR ${SeriesList[SeriesList.length - 1]}`
+      )
+    })
   })
 })
