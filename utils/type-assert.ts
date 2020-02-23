b315e555838a11711ce77dfe7dc6074e55ce0b03
nogic <24802730+nogic1008@users.noreply.github.com>
2020-02-23 19:51:02 +0900

    reafactor: change args type K -> K[]

--- utils/type-assert.ts
+++ utils/type-assert.ts
@@ -1,16 +1,17 @@
 export const hasProperty = <K extends string>(
   x: unknown,
-  name: K
-): x is { [M in K]: unknown } => x instanceof Object && name in x
+  ...names: K[]
+): x is { [M in K]: unknown } =>
+  x instanceof Object && names.every((n) => n in x)
 
 export const hasStringProperty = <K extends string>(
   x: unknown,
-  name: K
+  ...names: K[]
 ): x is { [M in K]: string } =>
-  hasProperty(x, name) && typeof x[name] === 'string'
+  hasProperty(x, ...names) && names.every((n) => typeof x[n] === 'string')
 
 export const hasNumberProperty = <K extends string>(
   x: unknown,
-  name: K
+  ...names: K[]
 ): x is { [M in K]: number } =>
-  hasProperty(x, name) && typeof x[name] === 'number'
+  hasProperty(x, ...names) && names.every((n) => typeof x[n] === 'number')
